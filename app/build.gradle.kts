2d6507abe617d2cf00918fab3b75a7953b39dafa
Santosh <santoshinchalakaranje@gmail.com>
2024-02-18 19:01:34 +0530

    added test cases for network

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,10 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
     testImplementation("junit:junit:4.13.2")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
+    testImplementation ("com.google.truth:truth:1.4.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -106,7 +109,8 @@ dependencies {
     // Coil
     implementation ("io.coil-kt:coil-compose:1.4.0")
 
-
+    //okhttp
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
 kapt {
     correctErrorTypes = true
---
+++ app/src/test/java/com/prplmnstr/bluetoothapi/network/NewApiTest.kt
@@ -0,0 +1,92 @@
+package com.prplmnstr.bluetoothapi.network
+
+
+
+
+
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.runBlocking
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import okio.buffer
+import okio.source
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class NewApiTest {
+
+    private lateinit var service: NewsApi
+    private lateinit var server: MockWebServer
+
+    @Before
+    fun setUp() {
+        server = MockWebServer()
+        service = Retrofit.Builder()
+            .baseUrl(server.url(""))
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NewsApi::class.java)
+
+
+    }
+
+    private fun enqueueMockResponse(
+        fileName: String
+    ) {
+        val inputStream = javaClass.classLoader!!.getResourceAsStream(fileName)
+        val source = inputStream.source().buffer()
+        val mockResponse = MockResponse()
+        mockResponse.setBody(source.readString(Charsets.UTF_8))
+        server.enqueue(mockResponse)
+
+    }
+
+    @Test
+    fun getTopHeadlines_sentRequest_receivedExpected() {
+        runBlocking {
+            enqueueMockResponse("newsresponse.json")
+            val responseBody = service.getTopHeadLines("us", 1)
+            val request = server.takeRequest()
+            assertThat(responseBody).isNotNull()
+            assertThat(request.path).isEqualTo("/v2/top-headlines?country=us&page=1&apiKey=5b7ec328f7a546588d759a822a207b32")
+
+
+        }
+    }
+
+    @Test
+    fun getTopHeadlines_receivedResponse_correctPageSize() {
+        runBlocking {
+            enqueueMockResponse("newsresponse.json")
+            val responseBody = service.getTopHeadLines("us", 1)
+            val articlesList = responseBody!!.articles
+            assertThat(articlesList.size).isEqualTo(20)
+
+        }
+    }
+
+    @Test
+    fun getTopHeadlines_receivedResponse_correctContent() {
+        runBlocking {
+            enqueueMockResponse("newsresponse.json")
+            val responseBody = service.getTopHeadLines("us", 1)
+            val articlesList = responseBody!!.articles
+            val article = articlesList[1]
+            assertThat(article.author).isEqualTo("Ian Livingston")
+
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
